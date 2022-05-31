95afe40cd471b7f2b264671d5fb1326fd512ed5c
komal-codentic <komal_yadav@codenticsoftware.com>
2022-05-31 18:23:16 +0530

    changed table name

--- app/models/TipForDayModel.js
+++ app/models/TipForDayModel.js
@@ -1,15 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
-    const TipForDayModel = sequelize.define(
-        'tipforday',
-        {
-            name: { type: Sequelize.STRING, notNull: true },
-            value: { type: Sequelize.STRING, notNull: true },
-        },
-        {
-            timestamps: true,
-            underscored: true,
-            tableName: 'tipforday',
-        }
-    );
-    return TipForDayModel;
+  const TipForDayModel = sequelize.define(
+    "tip_for_day",
+    {
+      name: { type: Sequelize.STRING, notNull: true },
+      value: { type: Sequelize.STRING, notNull: true },
+    },
+    {
+      timestamps: true,
+      underscored: true,
+      tableName: "tip_for_day",
+    }
+  );
+  return TipForDayModel;
 };
