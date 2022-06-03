df9aa236c7c57912d54c75281c146ec82fd0fead
Hiral0812 <77874590+Hiral0812@users.noreply.github.com>
2022-06-03 11:52:26 +0530

    updated auth function in routes

--- app/routes/NestedSubcategoryRoutes.js
+++ app/routes/NestedSubcategoryRoutes.js
@@ -1,11 +1,13 @@
 module.exports = (app) => {
-    const NestedsubcategoryController = require("../controllers/NestedSubcategoryController");
-    // const validationController = require('../controllers/ValidationController')
-    var router = require("express").Router();
-    router.get(
-        "/all-nestedsubcategory",
-        auth(),
-        NestedsubcategoryController.allnestedsubcategory
-    );
-    app.use("/", router);
+  const NestedsubcategoryController = require("../controllers/NestedSubcategoryController");
+  const auth = require("./middleware/auth.middleware");
+
+  // const validationController = require('../controllers/ValidationController')
+  var router = require("express").Router();
+  router.get(
+    "/all-nestedsubcategory",
+    auth(),
+    NestedsubcategoryController.allnestedsubcategory
+  );
+  app.use("/", router);
 };
