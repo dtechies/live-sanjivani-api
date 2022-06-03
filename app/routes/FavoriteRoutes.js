46928d1f1982ea795d6ca1e653167fd60169f5a7
Hiral0812 <77874590+Hiral0812@users.noreply.github.com>
2022-06-03 10:22:59 +0530

    development of add-user-favorites api

--- app/routes/FavoriteRoutes.js
+++ app/routes/FavoriteRoutes.js
@@ -1,10 +1,12 @@
 module.exports = (app) => {
-    const favoriteController = require('../controllers/FavoriteController');
-    // const validationController = require('../controllers/ValidationController')
-    var router = require('express').Router();
-    router.get('/user-favorites-list', [ favoriteController.userFavorites]);
-    router.post('/add-user-favorites', [ favoriteController.addFavorites]);
-    //router.post('/edit-user-favorites-list', [ favoriteController.editFavoritesList]);
-    
-    app.use('/', router);
+  const favoriteController = require("../controllers/FavoriteController");
+  const auth = require("./middleware/auth.middleware");
+
+  // const validationController = require('../controllers/ValidationController')
+  var router = require("express").Router();
+  router.get("/user-favorites-list", auth(), favoriteController.userFavorites);
+  router.post("/add-user-favorites", auth(), favoriteController.addFavorites);
+  //router.post('/edit-user-favorites-list', [ favoriteController.editFavoritesList]);
+
+  app.use("/", router);
 };
