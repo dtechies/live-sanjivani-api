cd19ce56ad11a316844c39af44c133be2bec625c
komal-codentic <komal_yadav@codenticsoftware.com>
2022-06-07 10:03:33 +0530

    Get user profile and changes in edit user profile

--- app/controllers/UserProfileController.js
+++ app/controllers/UserProfileController.js
@@ -2,39 +2,33 @@ const { UsersModel } = require("../imports");
 const constants = require("../imports").constants;
 const { S3 } = require("../imports");
 const dotenv = require("dotenv");
-let { jwt } = require("../imports");
 dotenv.config();
 
 exports.editUserProfile = async (req, res, next) => {
   const user_id = req.user_id;
-  const UserProfilePic = await UsersModel.findOne({
+  const UserProfileData = await UsersModel.findOne({
     where: { id: user_id },
   });
-  if (req.body.otp) {
-    if (req.body.otp == UserProfilePic.otp) {
-      if (UserProfilePic) {
-        try {
+  if (UserProfileData) {
+    try {
+      if (req.body.otp) {
+        if (req.body.otp == UserProfileData.otp) {
           if (req.files == null) {
-            const EditUserProfileData = await UsersModel.update(
-              {
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                gender: req.body.gender,
-                dob: req.body.dob,
-                email: req.body.email,
-                mob_no: req.body.mob_no,
-                language: req.body.language,
-              },
-              { where: { id: UserProfilePic.id } }
-            ).then((result) => {
-              return res.json(
-                constants.responseObj(true, 201, constants.messages.AddSuccess)
-              );
-            });
+            await updateUserProfile(user_id, req.body, UserProfileData).then(
+              (value) => {
+                return res.json(
+                  constants.responseObj(
+                    true,
+                    201,
+                    constants.messages.AddSuccess
+                  )
+                );
+              }
+            );
           } else {
             var params = {
               Bucket: "live-sanjivani",
-              Key: `userProfileImages/${UserProfilePic.image}`,
+              Key: `userProfileImages/${UserProfileData.image}`,
             };
             S3.deleteObject(params, function (err, data) {
               if (err) {
@@ -43,86 +37,27 @@ exports.editUserProfile = async (req, res, next) => {
                 console.log("sucessfully deleted images", data);
               }
             });
-            imageUpload(
+            await updateUserProfileData(
               req.files.image,
               req.files.image.name,
-              function (err, image) {
-                if (err) {
-                  return res.json(
-                    constants.responseObj(false, 500, error.errors[0].message)
-                  );
-                } else {
-                  try {
-                    let UserProfilePicData = {
-                      user_id: user_id,
-                      first_name: req.body.first_name,
-                      last_name: req.body.last_name,
-                      gender: req.body.gender,
-                      dob: req.body.dob,
-                      email: req.body.email,
-                      mob_no: req.body.mob_no,
-                      language: req.body.language,
-                      image: image.image,
-                    };
-                    const UserProfilePicUpdate = UsersModel.update(
-                      UserProfilePicData,
-                      {
-                        where: { id: UserProfilePic.id },
-                      }
-                    );
-
-                    if (UserProfilePicUpdate) {
-                      return res.json(
-                        constants.responseObj(
-                          true,
-                          201,
-                          constants.messages.AddSuccess
-                        )
-                      );
-                    } else {
-                      return res.json(
-                        constants.responseObj(
-                          false,
-                          500,
-                          constants.messages.SomethingWentWrong
-                        )
-                      );
-                    }
-                  } catch (error) {
-                    console.log(error, "error");
-                    return res.json(constants.responseObj(false, 500, error));
-                  }
-                }
-              }
-            );
+              user_id,
+              req.body,
+              UserProfileData
+            ).then((value) => {
+              return res.json(
+                constants.responseObj(true, 201, constants.messages.AddSuccess)
+              );
+            });
           }
-        } catch (error) {
-          console.log(error, "error");
-          return res.json(constants.responseObj(false, 500, error));
+        } else {
+          return res.json(constants.responseObj(true, 401, "Invalid Otp"));
         }
       } else {
-        imageUpload(
-          req.files.image,
-          req.files.image.name,
-          function (err, image) {
-            if (err) {
-              req.session.error = imports.constants.messages.InvalidFile;
-            } else {
-              try {
-                let UserProfileData = {
-                  user_id: user_id,
-                  first_name: req.body.first_name,
-                  last_name: req.body.last_name,
-                  gender: req.body.gender,
-                  dob: req.body.dob,
-                  email: req.body.email,
-                  mob_no: req.body.mob_no,
-                  language: req.body.language,
-                  otp: req.body.otp,
-                  image: image.image,
-                };
-                const UserProfile = UsersModel.create(UserProfileData);
-                if (UserProfile) {
+        if (UserProfileData) {
+          try {
+            if (req.files == null) {
+              await updateUserProfile(user_id, req.body, UserProfileData).then(
+                (value) => {
                   return res.json(
                     constants.responseObj(
                       true,
@@ -130,156 +65,98 @@ exports.editUserProfile = async (req, res, next) => {
                       constants.messages.AddSuccess
                     )
                   );
+                }
+              );
+            } else {
+              var params = {
+                Bucket: "live-sanjivani",
+                Key: `userProfileImages/${UserProfileData.image}`,
+              };
+              S3.deleteObject(params, function (err, data) {
+                if (err) {
+                  console.log(err, "err");
                 } else {
-                  return res.json(
-                    constants.responseObj(
-                      false,
-                      500,
-                      constants.messages.SomethingWentWrong
-                    )
-                  );
+                  console.log("sucessfully deleted images", data);
                 }
-              } catch (error) {
-                console.log(error, "error");
-                return res.json(constants.responseObj(false, 500, error));
-              }
-            }
-          }
-        );
-      }
-    } else {
-      return res.json(constants.responseObj(true, 401, "Invalid Otp"));
-    }
-  } else {
-    if (UserProfilePic) {
-      try {
-        if (req.files == null) {
-          const EditUserProfileData = await UsersModel.update(
-            {
-              first_name: req.body.first_name,
-              last_name: req.body.last_name,
-              gender: req.body.gender,
-              dob: req.body.dob,
-              email: req.body.email,
-              mob_no: req.body.mob_no,
-              language: req.body.language,
-            },
-            { where: { id: UserProfilePic.id } }
-          ).then((result) => {
-            return res.json(
-              constants.responseObj(true, 201, constants.messages.AddSuccess)
-            );
-          });
-        } else {
-          var params = {
-            Bucket: "live-sanjivani",
-            Key: `userProfileImages/${UserProfilePic.image}`,
-          };
-          S3.deleteObject(params, function (err, data) {
-            if (err) {
-              console.log(err, "err");
-            } else {
-              console.log("sucessfully deleted images", data);
-            }
-          });
-          imageUpload(
-            req.files.image,
-            req.files.image.name,
-            function (err, image) {
-              if (err) {
+              });
+              await updateUserProfileData(
+                req.files.image,
+                req.files.image.name,
+                user_id,
+                req.body,
+                UserProfileData
+              ).then((value) => {
                 return res.json(
-                  constants.responseObj(false, 500, error.errors[0].message)
+                  constants.responseObj(
+                    true,
+                    201,
+                    constants.messages.AddSuccess
+                  )
                 );
-              } else {
-                try {
-                  let UserProfilePicData = {
-                    user_id: user_id,
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    gender: req.body.gender,
-                    dob: req.body.dob,
-                    email: req.body.email,
-                    mob_no: req.body.mob_no,
-                    language: req.body.language,
-                    image: image.image,
-                  };
-                  const UserProfilePicUpdate = UsersModel.update(
-                    UserProfilePicData,
-                    {
-                      where: { id: UserProfilePic.id },
-                    }
-                  );
-
-                  if (UserProfilePicUpdate) {
-                    return res.json(
-                      constants.responseObj(
-                        true,
-                        201,
-                        constants.messages.AddSuccess
-                      )
-                    );
-                  } else {
-                    return res.json(
-                      constants.responseObj(
-                        false,
-                        500,
-                        constants.messages.SomethingWentWrong
-                      )
-                    );
-                  }
-                } catch (error) {
-                  console.log(error, "error");
-                  return res.json(constants.responseObj(false, 500, error));
-                }
-              }
-            }
-          );
-        }
-      } catch (error) {
-        console.log(error, "error");
-        return res.json(constants.responseObj(false, 500, error));
-      }
-    } else {
-      imageUpload(req.files.image, req.files.image.name, function (err, image) {
-        if (err) {
-          req.session.error = imports.constants.messages.InvalidFile;
-        } else {
-          try {
-            let UserProfileData = {
-              user_id: user_id,
-              first_name: req.body.first_name,
-              last_name: req.body.last_name,
-              gender: req.body.gender,
-              dob: req.body.dob,
-              email: req.body.email,
-              mob_no: req.body.mob_no,
-              language: req.body.language,
-              otp: req.body.otp,
-              image: image.image,
-            };
-            const UserProfile = UsersModel.create(UserProfileData);
-            if (UserProfile) {
-              return res.json(
-                constants.responseObj(true, 201, constants.messages.AddSuccess)
-              );
-            } else {
-              return res.json(
-                constants.responseObj(
-                  false,
-                  500,
-                  constants.messages.SomethingWentWrong
-                )
-              );
+              });
             }
           } catch (error) {
             console.log(error, "error");
             return res.json(constants.responseObj(false, 500, error));
           }
         }
-      });
+      }
+    } catch (error) {
+      console.log(error, "error");
+      return res.json(constants.responseObj(false, 500, error));
     }
+  } else {
+    return res.json(constants.responseObj(false, 404, "User Not Found"));
   }
 };
+
+async function updateUserProfile(body, UserProfileData) {
+  await UsersModel.update(
+    {
+      first_name: body.first_name,
+      last_name: body.last_name,
+      gender: body.gender,
+      dob: body.dob,
+      email: body.email,
+      mob_no: body.mob_no,
+      language: body.language,
+    },
+    { where: { id: UserProfileData.id } }
+  );
+}
+async function updateUserProfileData(
+  image,
+  name,
+  user_id,
+  body,
+  UserProfileData
+) {
+  imageUpload(image, name, function (err, image) {
+    if (err) {
+      return false;
+    } else {
+      try {
+        let UserProfilePicData = {
+          user_id: user_id,
+          first_name: body.first_name,
+          last_name: body.last_name,
+          gender: body.gender,
+          dob: body.dob,
+          email: body.email,
+          mob_no: body.mob_no,
+          language: body.language,
+          image: image.image,
+        };
+        const UserProfilePicUpdate = UsersModel.update(UserProfilePicData, {
+          where: { id: UserProfileData.id },
+        });
+      } catch (error) {
+        console.log(error, "error");
+        return false;
+      }
+    }
+  });
+}
 
 function imageUpload(image, imgAttachement, cb) {
   var params = {
@@ -299,3 +176,21 @@ function imageUpload(image, imgAttachement, cb) {
     }
   });
 }
+exports.getUserProfileData = async (req, res, next) => {
+  try {
+    const user_id = req.user_id;
+    const UserProfileData = await UsersModel.findOne({
+      where: { id: user_id },
+    });
+    return res.json(
+      constants.responseObj(true, 201, constants.messages.DataFound, false, {
+        UserProfileData,
+      })
+    );
+  } catch (error) {
+    console.log(error, "error");
+    return res.json(
+      constants.responseObj(false, 500, constants.messages.SomethingWentWrong)
+    );
+  }
+};
