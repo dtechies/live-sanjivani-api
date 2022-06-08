0ce04b629b59c5e9544f7a928e67667ae7d865a7
swati-2904 <swati_maniyar@codenticsoftware.com>
2022-06-08 15:04:09 +0530

    Added one field is_done in medication-list API

--- app/models/index.js
+++ app/models/index.js
@@ -3,12 +3,11 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+  process.env.DB_PASSWORD, {
     dialect: "mysql",
     host: "localhost",
     dialectOptions: {
-      socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+      // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
       // socketPath: ""
     },
   }
