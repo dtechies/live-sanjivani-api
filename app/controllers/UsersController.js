e4284c811350a58fd0e480ca3ef5faa0f6e9111c
Hiral0812 <77874590+Hiral0812@users.noreply.github.com>
2022-07-21 18:25:07 +0530

    development of /get-refresh-token api

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -42,10 +42,26 @@ exports.registerUser = async (req, res, next) => {
         },
         secretKey,
         {
-          expiresIn: "24h",
+          expiresIn: "1m",
         }
       );
+      const refreshToken = jwt.sign(
+        {
+          mob_no: user.mob_no,
+          user_id: user.id,
+        },
+        refreshTokenSecretKey,
+        {
+          expiresIn: "4m",
+        }
+      );
+      const tokenTime = jwt.verify(token, secretKey);
+      const refreshTokenTime = jwt.verify(refreshToken, refreshTokenSecretKey);
+
       user.dataValues.token = token;
+      user.dataValues.refreshToken = refreshToken;
+      user.dataValues.tokenTime = tokenTime.exp;
+      user.dataValues.refreshTokenTime = refreshTokenTime.exp;
       return res.json(
         constants.responseObj(
           true,
@@ -92,7 +108,8 @@ exports.usersLogin = async (req, res, next) => {
   }
   let i18n = languageFunc(user.language);
   if (user.otp == Otp) {
-    const secretKey = process.env.SECRET_JWT || "theseissecret";
+    const secretKey = process.env.SECRET_JWT;
+    const refreshTokenSecretKey = process.env.REFRESH_SECRET_KEY;
     const token = jwt.sign(
       {
         mob_no: user.mob_no,
@@ -100,10 +117,26 @@ exports.usersLogin = async (req, res, next) => {
       },
       secretKey,
       {
-        expiresIn: "24h",
+        expiresIn: "1m",
       }
     );
+    const refreshToken = jwt.sign(
+      {
+        mob_no: user.mob_no,
+        user_id: user.id,
+      },
+      refreshTokenSecretKey,
+      {
+        expiresIn: "4m",
+      }
+    );
+    const tokenTime = jwt.verify(token, secretKey);
+    const refreshTokenTime = jwt.verify(refreshToken, refreshTokenSecretKey);
+
     user.dataValues.token = token;
+    user.dataValues.refreshToken = refreshToken;
+    user.dataValues.tokenTime = tokenTime.exp;
+    user.dataValues.refreshTokenTime = refreshTokenTime.exp;
     return res.json(
       constants.responseObj(true, 200, i18n.__(`UserLogin`), false, {
         user,
@@ -135,6 +168,61 @@ exports.addEditPlayerId = async (req, res, next) => {
     // return res.json(constants.responseObj(false, 500, error));
     return res.json(
       constants.responseObj(false, 500, i18n.__(`DuplicateNumber`))
+    );
+  }
+};
+
+exports.generateTokenFromRefreshToken = async (req, res, next) => {
+  try {
+    const refreshTokenSecretKey = process.env.REFRESH_SECRET_KEY;
+    const secretKey = process.env.SECRET_JWT;
+    const decoded = jwt.verify(req.body.refresh_token, refreshTokenSecretKey);
+    const token = jwt.sign(
+      {
+        mob_no: decoded.mob_no,
+        user_id: decoded.user_id,
+      },
+      secretKey,
+      {
+        expiresIn: "1m",
+      }
+    );
+    const refreshToken = jwt.sign(
+      {
+        mob_no: decoded.mob_no,
+        user_id: decoded.user_id,
+      },
+      refreshTokenSecretKey,
+      {
+        expiresIn: "4m",
+      }
+    );
+    const tokenTime = jwt.verify(token, secretKey);
+    const refreshTokenTime = jwt.verify(refreshToken, refreshTokenSecretKey);
+    let user = await UsersModel.findOne({
+      where: {
+        id: decoded.user_id,
+      },
+    });
+    user.dataValues.token = token;
+    user.dataValues.refreshToken = refreshToken;
+    user.dataValues.tokenTime = tokenTime.exp;
+    user.dataValues.refreshTokenTime = refreshTokenTime.exp;
+    if (token) {
+      return res.json(
+        constants.responseObj(true, 200, constants.messages.Token, false, {
+          user,
+        })
+      );
+    } else {
+      return res.json(
+        constants.responseObj(false, 401, constants.messages.InvalidToken)
+      );
+    }
+  } catch (err) {
+    console.log(err, "errror");
+    return res.json(
+      constants.responseObj(false, 401, constants.messages.InvalidToken)
     );
   }
 };
