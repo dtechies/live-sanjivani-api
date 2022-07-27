ffeb3ac1f79339cdcd45d04f1ac91d03bc770c10
Hiral0812 <77874590+Hiral0812@users.noreply.github.com>
2022-07-27 15:29:48 +0530

    added add-data, db-clean scripts and send otp in register-user api

---
+++ app/db-connection/add-data.js
@@ -0,0 +1,13 @@
+const seeder = require("../seeder/tableData");
+
+const addData = async () => {
+  seeder.addCategory();
+  seeder.addHelpSupport();
+  seeder.addMedicineStrength();
+  seeder.addReminderFrequencyData();
+  seeder.addReminderTimeData();
+  seeder.addSymptomData();
+  seeder.addTipForDayData();
+};
+
+addData();
