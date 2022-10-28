92e1f3dad733e50d7d99c9cb1d774069ab596667
Darshan Borad <77874795+darshanborad07@users.noreply.github.com>
2022-10-28 12:10:01 +0530

    Updated admin login code

--- app/controllers/OTPController.js
+++ app/controllers/OTPController.js
@@ -84,7 +84,7 @@ exports.getOTP = async (req, res, next) => {
 exports.sendOTP = async (req, res, next) => {
   const otpMethod = req.body.otpMethod || 'sms';
   let random = Math.floor(1000 + Math.random() * 9000);
-  if(req.body.mob_no == '9898989898'){
+  if(req.body.mob_no == '9876543210'){
     return res.send(constants.responseObj(true, 200, constants.messages.successSendOtp,{otp:5432}))
   }
    
